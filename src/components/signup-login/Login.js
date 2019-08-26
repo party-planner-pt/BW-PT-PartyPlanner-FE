3241e9ade0d23662103be45425ec64bafaec4b5c
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-25 20:58:29 -0400

    Made changes to form

--- src/components/signup-login/Login.js
+++ src/components/signup-login/Login.js
@@ -1,111 +1,76 @@
-import React, { useState, useEffect } from "react";
-import { Form, Field, withFormik } from "formik";
-import * as yup from "yup";
+import React from "react";
+import { withFormik, Formik } from "formik";
+import * as Yup from "yup";
 import axios from "axios";
+import TextField from "@material-ui/core/TextField";
+import * as EmailValidator from "email-validator";
 
-const Login = ({ errors, touched, status }) => {
-  const [signup, setSignIn] = useState([]);
-  console.log(signup);
+const LoginForm = () => (
+  <Formik
+    initialValues={{ email: "", password: "" }}
+    onSubmit={(values, { setSubmitting }) => {
+      setTimeout(() => {
+        console.log("Logging in", values);
+        setSubmitting(false);
+      }, 500);
+    }}
+    validationSchema={Yup.object().shape({
+      // firstname: Yup.string().required("Required"),
+      // lastname: Yup.string().required("Required"),
+      email: Yup.string()
+        .email()
+        .required("Required"),
+      password: Yup.string()
+        .required("No password provided.")
+        .min(8, "Password is too short - should be 8 chars minimum.")
+        .matches(/(?=.*[0-9])/, "Password must contain a number.")
+    })}
+  >
+    {props => {
+      const {
+        values,
+        touched,
+        errors,
+        isSubmitting,
+        handleChange,
+        handleBlur,
+        handleSubmit
+      } = props;
+      return (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="text"
+            placeholder="Enter your email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={errors.email && touched.email && "error"}
+          />
+          {errors.email && touched.email && (
+            <div className="feedback">{errors.email}</div>
+          )}
+          <label htmlFor="email">Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={errors.password && touched.password && "error"}
+          />
+          {errors.password && touched.password && (
+            <div className="feedback">{errors.password}</div>
+          )}
+          <button type="submit" disabled={isSubmitting}>
+            Login
+          </button>
+        </form>
+      );
+    }}
+  </Formik>
+);
 
-  useEffect(() => {
-    if (status) {
-      setSignIn([...signup, status]);
-    }
-  }, [status]);
-
-  return (
-    // <div className="form-container">
-    <Form>
-      {touched.First_name && errors.First_name && (
-        <p className="error">{errors.First_names}</p>
-      )}
-      <Field
-        component="input"
-        type="text"
-        name="First_name"
-        placeholder="First name"
-      />
-      {touched.Last_name && errors.Last_name && (
-        <p className="error">{errors.Last_name}</p>
-      )}
-      <Field
-        component="input"
-        type="text"
-        name="Last_name"
-        placeholder="Last name"
-      />
-      {touched.Your_email_address && errors.Your_email_address && (
-        <p className="error">{errors.Your_email_address}</p>
-      )}
-      <Field
-        component="input"
-        type="text"
-        name="Your_email_address"
-        placeholder="Email"
-      />
-      {touched.Choose_a_password && errors.Choose_a_passwords && (
-        <p className="error">{errors.Choose_a_password}</p>
-      )}
-      <Field
-        component="input"
-        type="text"
-        name=" Choose a password"
-        placeholder="Password"
-      />
-      {touched.TermofService && errors.TermofService && (
-        <p className="error">{errors.TermofService}</p>
-      )}
-      <label className="checkbox-container">
-        <Field type="checkbox" name="TermofService" />
-        I agree to the terms of Service and provacy policy.
-        <span className="checkmark" />
-      </label>
-      <button>Login</button>
-    </Form>
-
-    // </div>
-  );
-};
-
-const FormikLogin = withFormik({
-  mapPropsToValues: ({ name, email, password, TermofService }) => {
-    return {
-      name: name || "",
-      email: email || "",
-      password: password || "",
-      TermofService: TermofService || false
-    };
-  },
-  validationSchema: yup.object().shape({
-    name: yup.string().required("Please enter your name."),
-    email: yup
-      .string()
-      .email("Please create an email")
-      .required("You really need it"),
-    password: yup
-      .string()
-      .min(4, "Password must be atleast 4 characters long")
-      .required("Password is required"),
-    TermofService: yup
-      .boolean()
-      .oneOf([true], "Please agree to the terms")
-      .required()
-  }),
-
-  handleSubmit(values, { setStatus, resetForm, setErrors }) {
-    if (values.email === "waffles@syrup.com") {
-      setErrors({ email: "Email is already taken" });
-    } else {
-      axios
-        .post("https://potluck-planner-bw.herokuapp.com/users/login", values)
-        .then(res => {
-          console.log("handleSubmit: then: res: ", res);
-          setStatus(res.data);
-          resetForm();
-        })
-        .catch(err => console.error("handleSubmit: catch: err: ", err));
-    }
-  }
-})(Login);
-
-export default FormikLogin;
+export default LoginForm;
