e6c69a124984a69cd854d5b0600202dbc6d50ada
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-26 18:50:15 -0400

    realized it pull didn't bring over master changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import "./App.css";
 import Login from "./components/signup-login/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
