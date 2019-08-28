22078e621f908e38b1c9fedf77f8e060e6bd9a11
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-28 00:31:25 -0400

    commit changes to dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import FullWidthTabs from "./components/signup-login/SignupLoginTab";
-
+import StartMeal from "./src/components/create-potluck/user-dashboard";
 function App() {
   return (
     <div className="App">
       <FullWidthTabs />
+      <StartMeal />
     </div>
   );
 }
