74fa52e5e1d9762895c10994110d87b042b6b5f3
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-28 22:05:54 -0400

    Login updated now should work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
 import FullWidthTabs from "./components/signup-login/SignupLoginTab";
-import StartMeal from "./src/components/create-potluck/user-dashboard";
+import CreatePotluckForm from "./components/create-potluck/CreatePotluckForm";
+import Dashboard from "./components/create-potluck/user-dashboard/UserDashboard";
+import Volunteer from "./components/Volunteer";
+import { Route } from "react-router-dom";
+import Login from "./components/signup-login/Login";
 function App() {
   return (
     <div className="App">
       <FullWidthTabs />
-      <StartMeal />
+      {/* <Dashboard />
+      <CreatePotluckForm />
+      <Volunteer /> */}
+      <Route path="/login" component={Login} />
     </div>
   );
 }
--- src/components/signup-login/SignUp.js
+++ src/components/signup-login/SignUp.js
@@ -1,22 +1,17 @@
-import React from 'react'
-import { Field, withFormik, Form } from 'formik';
-import * as Yup from 'yup'
-import axios from 'axios'
-import { TextField } from 'formik-material-ui';
-import { SignUpButton, SignUpWrapper, StyledForm } from './SignUpLoginStyles'
+import React from "react";
+import { Field, withFormik, Form } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { TextField } from "formik-material-ui";
+import { SignUpButton, SignUpWrapper, StyledForm } from "./SignUpLoginStyles";
 import Button from "@material-ui/core/Button";
 
 const SignUp = props => {
+  const { touched, errors, handleSubmit } = props;
 
-  const {
-    touched,
-    errors,
-    handleSubmit
-  } = props;
-  
   return (
     <div>
-      <StyledForm onSubmit={handleSubmit}> 
+      <StyledForm onSubmit={handleSubmit}>
         <SignUpWrapper>
           <Field
             type="text"
@@ -27,8 +22,10 @@ const SignUp = props => {
             margin="normal"
             variant="outlined"
           />
-          {touched.name && errors.name && <p className="error">{errors.name}</p>}
-          
+          {touched.name && errors.name && (
+            <p className="error">{errors.name}</p>
+          )}
+
           <Field
             type="text"
             name="username"
@@ -38,7 +35,9 @@ const SignUp = props => {
             margin="normal"
             variant="outlined"
           />
-          {touched.username && errors.username && <p className="error">{errors.username}</p>}
+          {touched.username && errors.username && (
+            <p className="error">{errors.username}</p>
+          )}
 
           <Field
             type="email"
@@ -49,8 +48,9 @@ const SignUp = props => {
             margin="normal"
             variant="outlined"
           />
-          {touched.email && errors.email && <p className="error">{errors.email}</p>}
-  
+          {touched.email && errors.email && (
+            <p className="error">{errors.email}</p>
+          )}
 
           <Field
             type="password"
@@ -61,44 +61,46 @@ const SignUp = props => {
             margin="normal"
             variant="outlined"
           />
-          {touched.password && errors.password && <p className="error">{errors.password}</p>}
-          
+          {touched.password && errors.password && (
+            <p className="error">{errors.password}</p>
+          )}
         </SignUpWrapper>
         <button variant="contained" color="primary" component="button">
-            Sign Up
+          Sign Up
         </button>
         <p>Already registered? Login</p>
       </StyledForm>
     </div>
-  )
-}
-const FormikSignUp = withFormik ({
-  mapPropsToValues({name, username, email, password}) {
+  );
+};
+const FormikSignUp = withFormik({
+  mapPropsToValues({ name, username, email, password }) {
     return {
       name: name || "",
       username: username || "",
       email: email || "",
       password: password || ""
-    }
+    };
   },
 
   validationSchema: Yup.object().shape({
-    name: Yup.string().required('required'),
-    username: Yup.string().required('required'), // must not contain spaces
-    email: Yup.string().email().required('required'),
-    password: Yup.string().required('required'), 
+    name: Yup.string().required("required"),
+    username: Yup.string().required("required"), // must not contain spaces
+    email: Yup.string()
+      .email()
+      .required("required"),
+    password: Yup.string().required("required")
   }),
 
   handleSubmit(values, { setStatus }) {
     axios
-      .post('https://potluck-planner-bw.herokuapp.com/users/register', values)
+      .post("https://potluck-planner-bw.herokuapp.com/users/register", values)
       .then(res => {
-        console.log(res.data)
-        setStatus(res.data)
+        console.log(res.data);
+        setStatus(res.data);
       })
-      .catch(err => console.error(err))
+      .catch(err => console.error(err));
   }
-})(SignUp)
+})(SignUp);
 
-export default FormikSignUp
-
+export default FormikSignUp;
